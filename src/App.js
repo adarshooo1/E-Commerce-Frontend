527fa6c2f6c2a42884b64bdf3c31cda84396b0b3
Adarsh Singh <adarshsinghak001@gmail.com>
2023-08-11 15:13:30 +0530

    E-Commerce website Project Setup, Additionally added layout of dummy products, Category Filter and a Navbar Component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Counter } from './features/counter/Counter';
+import './App.css';
+import ProductList from './features/product/ProductList';
+
+function App() {
+  return (
+    <div className="App">
+     <ProductList></ProductList>
+    </div>
+  );
+}
+
+export default App;
