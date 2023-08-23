1a660469aae54ab432bcbd419f1ef697c219e87f
Adarsh Singh <adarshsinghak001@gmail.com>
2023-08-23 21:02:48 +0530

    E-Commerce Part 5:Added filtering option according to the products Category and Brand selected with test.js which is run by json-server installed globally with commend npx json-server --watch data.json --port 1010 for fetching data from the json server.

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,6 +1,23 @@
-export function fetchCount(amount = 1) {
+export function fetchAllProducts() {
   return new Promise(async (resolve) => {
-    const response = await fetch("http://localhost:8080");
+    // Todo: We will not hard-code server URL here.
+    const response = await fetch("http://localhost:1010/products");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function fetchProductsByFilters(filter) {
+  // Todo: On server we will support multiple values.
+  let queryString = "";
+  for (let key in filter) {
+    queryString += `${key}=${filter[key]}&`
+  }
+  return new Promise(async (resolve) => {
+    // Todo: We will not hard-code server URL here.
+    const response = await fetch(
+      "http://localhost:1010/products?" + queryString
+    );
     const data = await response.json();
     resolve({ data });
   });
--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -1,9 +1,10 @@
+import Navbar from "../features/navbar/Navbar";
 import ProductDetail from "../features/product/components/ProductDetail";
 
 export default function ProjectDetailPage() {
   return (
-    <div>
+    <Navbar>
       <ProductDetail></ProductDetail>
-    </div>
+    </Navbar>
   );
 }
