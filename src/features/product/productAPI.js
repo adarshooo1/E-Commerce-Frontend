8ef617e597114693fefe92f450c67d559ebcc758
Adarsh Singh <adarshsinghak001@gmail.com>
2023-09-17 21:08:14 +0530

    E-Commerce Frontend Part:27 , Sync With frontend Part:9, Let's deploy this backend Part 1

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,7 +1,7 @@
 export function fetchProductById(id) {
   return new Promise(async (resolve) => {
     // Todo: We will not hard-code server URL here.
-    const response = await fetch("http://localhost:1010/products/" + id);
+    const response = await fetch("/products/" + id);
     const data = await response.json();
     resolve({ data });
   });
@@ -9,7 +9,7 @@ export function fetchProductById(id) {
 
 export function createProduct(product) {
   return new Promise(async (resolve) => {
-    const response = await fetch("http://localhost:1010/products/", {
+    const response = await fetch("/products/", {
       method: "POST",
       body: JSON.stringify(product),
       headers: { "content-type": "application/json" },
@@ -22,7 +22,7 @@ export function createProduct(product) {
 export function updateProduct(update) {
   return new Promise(async (resolve) => {
     const response = await fetch(
-      "http://localhost:1010/products/" + update.id,
+      "/products/" + update.id,
       {
         method: "PATCH",
         body: JSON.stringify(update),
@@ -59,7 +59,7 @@ export function fetchProductsByFilters(filter, sort, pagination, admin) {
   return new Promise(async (resolve) => {
     // Todo: We will not hard-code server URL here.
     const response = await fetch(
-      "http://localhost:1010/products?" + queryString
+      "/products?" + queryString
     );
     const data = await response.json();
     const totalItems = await response.headers.get("X-TOTAL-COUNT");
@@ -70,7 +70,7 @@ export function fetchProductsByFilters(filter, sort, pagination, admin) {
 export function fetchCategories() {
   return new Promise(async (resolve) => {
     // Todo: We will not hard-code server URL here.
-    const response = await fetch("http://localhost:1010/categories");
+    const response = await fetch("/categories");
     const data = await response.json();
     resolve({ data });
   });
@@ -79,7 +79,7 @@ export function fetchCategories() {
 export function fetchBrands() {
   return new Promise(async (resolve) => {
     // Todo: We will not hard-code server URL here.
-    const response = await fetch("http://localhost:1010/brands");
+    const response = await fetch("/brands");
     const data = await response.json();
     resolve({ data });
   });
